a1229bdcc93f8370d38d6367196de8d988fa73c0
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2023-06-21 16:32:54 +0800

    Update Ch5_conditionMethod.py

--- Ch5_conditionMethod.py
+++ Ch5_conditionMethod.py
@@ -7,13 +7,13 @@ def main():
     print("change the student's grade to score")
     score = eval (input("student's score:"))
 
-    if (score <= 100 & score >= 90):
+    if (score <= 100 and score >= 90):
         grade = "A"
-    elif(score <=89  & score >= 80):
+    elif(score < 90  and score >= 80):
         grade = "B"
-    elif(score <=79  & score >= 70):
+    elif(score < 80  and score >= 70):
         grade = "C"
-    elif(score <=69  & score >= 60):
+    elif(score < 70  and score >= 60):
         grade = "D"
     else:
         grade = "F"
